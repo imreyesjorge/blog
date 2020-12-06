cfea25343e22d0f93a1c43bd202d3b895774bcf7
Jorge Reyes <reyestrevjorge@gmail.com>
2020-12-05 22:18:33 -0600

    feat: Basic structure
    Created the project with yarn, next and tailwind. Also added postcss and
    autoprefixer dependecies.

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+const index = () => {
+    return(
+        <>
+            <Head>
+                <title>
+                    Kings – Blog
+                </title>
+            </Head>
+        </>
+    )
+}
+
+export default index
